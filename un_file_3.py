13f94f4e3c63cd7aec0a83a5f644048e0b3eda84
Garry test <garry_79@mail.ru>
2023-03-02 00:12:40 +0300

    Last commit

--- un_file_3.py
+++ un_file_3.py
@@ -1,16 +1,13 @@
 class FilesParser:
 
     def make_list_with_metadata_path(self, path):
-        with open(path) as f:
+        with open(path, encoding='utf-8') as f:
             strings = f.readlines()
         return [path.split('/')[-1], len(strings)] + [el.strip() for el in strings]       
 
 if __name__ == '__main__':
     file_class = FilesParser()
-    file1 = r'https://raw.githubusercontent.com/netology-code/py-homework-basic-files/master/2.4.files/sorted/1.txt'
-    file2 = r'https://raw.githubusercontent.com/netology-code/py-homework-basic-files/master/2.4.files/sorted/2.txt'
-    file3 = r'https://raw.githubusercontent.com/netology-code/py-homework-basic-files/master/2.4.files/sorted/3.txt'
-    list_of_paths = [file1, file2, file3]
+    list_of_paths = ['1.txt', '2.txt', '3.txt']
     list_of_files = []
     for path in list_of_paths:
         file = file_class.make_list_with_metadata_path(path)
